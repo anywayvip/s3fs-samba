7d5c278c43abd8690a9d794f461b7dd141593ba6
anywayvip <122186319+anywayvip@users.noreply.github.com>
2024-11-21 00:33:03 +0800

    Update and rename start-2.sh to start.sh

---
+++ start.sh
@@ -0,0 +1,25 @@
+echo "$AWSKEY:$AWSSECRET" >> /etc/passwd-s3fs
+chmod 600 /etc/passwd-s3fs
+useradd -ms /bin/bash $SMBUSER
+(echo $SMBPASS; echo $SMBPASS) | smbpasswd -a -s $SMBUSER
+echo "" > /etc/samba/smb.conf
+
+echo Buckets are $BUCKET
+for B in $BUCKET; do
+	echo "Configuring folders for $B"
+	mkdir /mnt/s3fs-$B; 
+	mkdir /mnt/s3fs-$B-tmp; 
+	chmod -R 777 /mnt/s3fs-$B-tmp; 
+	chown -R $SMBUSER /mnt/s3fs-$B
+	sed "s/_BUCKET_/$B/g" /etc/samba/smb.conf.template >> /etc/samba/smb.conf
+done
+
+echo "$SMBUSER" >> /etc/samba/smb.conf
+service smbd start
+
+for B in $BUCKET; do
+	echo "Starting s3fs for $B"
+    s3fs "$B" /mnt/s3fs-"$B" -o storage_class=standard,host="$S3_HOST",allow_other,rw,umask=000,noatime,use_cache=/mnt/s3fs-"$B"-tmp,dbglevel=warn -f > /var/log/s3fs-"$B".log.txt &
+done
+
+tail /dev/null -f
